0b50219fd3d9c75070f4dfb48354647d4374c09c
Timothy Pfafman <tim@pfafman.com>
2016-04-06 16:09:54 -0700

    clean up

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: "pfafman:dc",
   summary: "Fast n-dimensional filtering and grouping of records.",
-  version: "2.1.0_23",
+  version: "2.1.0_24",
   git: "https://github.com/pfafman/meteor-dc.git"
 });
 
 
 Package.onUse(function (api) {
-  api.versionsFrom("METEOR@1.0");
+  api.versionsFrom('1.0.0');
 
   api.use('d3js:d3', 'client');
-  api.use('pfafman:crossfilter', 'client');
+  api.use('pfafman:crossfilter@1.4.0', 'client');
 
   api.addFiles(
     [
@@ -21,5 +21,4 @@ Package.onUse(function (api) {
     , 'client'
   );
 
-  api.export('dc', 'client');
 });
